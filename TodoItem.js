eb2eb356f14cadcb19906855c4d4733fd08b549a
HermineHakobyan <106839268+HermineHakobyan@users.noreply.github.com>
2023-05-18 17:37:02 +0400

    Todo with react

---
+++ TodoItem.js
@@ -0,0 +1,22 @@
+import "./TodoItem.css";
+function TodoItem({todo, onChange, onDelete}){
+    return (
+        <div>
+            <label>
+                <input type="checkbox" checked={todo.isCompleted} onChange={(e)=>{
+                    
+                    onChange({
+                        ...todo,
+                        isCompleted: e.target.checked
+                    })
+                }}/>
+                {todo.text}
+                <button onClick={()=>{
+                    onDelete(todo);
+                }}
+            >X</button>
+            </label>
+        </div>
+    )
+}
+export default TodoItem;
